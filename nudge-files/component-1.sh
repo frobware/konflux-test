948a44d0cd1b81a78712038f304979c70b305246
Andrew McDermott <aim@frobware.com>
2025-08-07 19:35:19 +0100

    Trigger Component B build to test updated CEL expressions
    Modified nudge-files/component-1.sh to trigger Component B build
    which should now detect this change due to the updated CEL expression
    that includes f.startsWith("nudge-files/").

--- nudge-files/component-1.sh
+++ nudge-files/component-1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Component 1 image reference - updated by Konflux nudging
-export COMPONENT_1_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/amcdermo-tenant/konflux-test-c794b@sha256:337734355c538af1ac269a8088405871fb7a7230f56c83210cfcb50a374e5b54"
+export COMPONENT_1_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/amcdermo-tenant/konflux-test-c794b@sha256:337734355c538af1ac269a8088405871fb7a7230f56c83210cfcb50a374e5b54"# Trigger component B build to test updated CEL expressions
