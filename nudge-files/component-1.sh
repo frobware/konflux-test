5634309a4cf481d46c0a9b27ba345a4ed7356528
Andrew McDermott <aim@frobware.com>
2025-08-07 18:15:48 +0100

    Switch nudging approach to match BPFman pattern
    Change from Dockerfile-based nudging to shell script-based nudging:
    - Update nudge-files/component-1.sh with correct digest format
    - Change build-nudge-files annotation to target .*.sh files
    - Use current Component A digest from latest successful build
    
    This matches BPFman's working nudging pattern where PRs update
    shell scripts with export statements rather than Dockerfiles.

--- nudge-files/component-1.sh
+++ nudge-files/component-1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Component 1 image reference - updated by Konflux nudging
-export COMPONENT_1_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/amcdermo-tenant/konflux-test-component:latest'
+export COMPONENT_1_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/amcdermo-tenant/konflux-test-c794b@sha256:10526425e1a5b8a568734c65206130696e040c47e1e064d6faea2932fa1b976b"
