0558564e7ab69343dcedb6328fb5f492b67786dc
Andrew McDermott <aim@frobware.com>
2025-08-07 18:35:43 +0100

    Restrict nudging to shell scripts only
    Remove image digest reference from Dockerfile so Renovate will only
    update the shell script specified in build-nudge-files annotation:
    - Removed ARG COMPONENT_A_IMAGE from Dockerfile
    - Updated nudge-files/component-1.sh with current digest
    - Nudging should now only target .sh files, not Dockerfiles
    
    This matches the intended BPFman pattern where nudging updates
    script files with export statements rather than Dockerfile ARGs.

--- nudge-files/component-1.sh
+++ nudge-files/component-1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Component 1 image reference - updated by Konflux nudging
-export COMPONENT_1_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/amcdermo-tenant/konflux-test-c794b@sha256:10526425e1a5b8a568734c65206130696e040c47e1e064d6faea2932fa1b976b"
+export COMPONENT_1_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/amcdermo-tenant/konflux-test-c794b@sha256:337734355c538af1ac269a8088405871fb7a7230f56c83210cfcb50a374e5b54"
