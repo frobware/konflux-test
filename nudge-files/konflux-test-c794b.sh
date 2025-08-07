071b3ea9e7100b8ea836de0892fcf5504d9fecae
Andrew McDermott <aim@frobware.com>
2025-08-07 17:08:11 +0100

    Bootstrap nudge file for konflux-test-c794b component

---
+++ nudge-files/konflux-test-c794b.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# This file is automatically updated by Konflux when konflux-test-c794b builds
+KONFLUX_TEST_C794B_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/amcdermo-tenant/konflux-test-c794b@sha256:placeholder-will-be-updated-by-konflux"
